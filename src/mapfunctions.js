d88cd17bc3d4baa176d7e11a4d396e6150930f1d
Sven Klomp <mail@klomp.eu>
2018-04-05 20:20:01 +0200

    Initial commit

---
+++ src/mapfunctions.js
@@ -0,0 +1,113 @@
+// Initialise map
+var map = L.map('map').fitBounds([ [52.40163, 10.16552], [52.42210, 10.20132] ]); 
+searchresultlayer = L.geoJSON().addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxNativeZoom: 19,
+  maxZoom: 22,
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+// Dorferlebnispfad
+
+var layerDorferlebnispfad;
+
+var showDorferlebnispfad = function(show) {
+  if (show) {
+    if (layerDorferlebnispfad === undefined) {
+      layerDorferlebnispfad = L.geoJSON(null, {
+        pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, {icon: iconDorferlebnispfad});
+        }
+      });
+      
+      layerDorferlebnispfad.bindTooltip(function(layer){
+        return layer.feature.properties.tags.name;
+      });
+      layerDorferlebnispfad.bindPopup(function(layer){
+        var content ="";
+        content = content + '<b>' + layer.feature.properties.tags.name + '</b><br>';
+        content = content + layer.feature.properties.tags.description + '<br>';
+        content = content + '<br>';
+        content = content + 'Weitere Details unter <a href="' + layer.feature.properties.tags.website + '">'+ layer.feature.properties.tags.website + '</a>';
+        return content
+      });
+      
+      
+      $.getJSON( createOverpassQuery(), function(json_data ) {
+          var geojson = osmtogeojson(json_data);
+          layerDorferlebnispfad.addData(geojson);
+        });
+      
+      
+      
+    }
+    map.addLayer(layerDorferlebnispfad);
+  } else {
+    if (layerDorferlebnispfad !== undefined) {
+      map.removeLayer(layerDorferlebnispfad);
+    }
+  }
+}
+
+var iconDorferlebnispfad = L.icon({
+  iconUrl: 'images/dorferlebnispfad.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  shadowUrl: 'images/dorferlebnispfad_schatten.png',
+  shadowSize: [32, 32],
+  shadowAnchor: [8, 32],
+  popupAnchor: [0, -32]
+});
+
+var createOverpassQuery = function(selectors) {
+  var base_url= "http://overpass-api.de/api/interpreter?data="
+  var area = "52.40163, 10.16552, 52.42210, 10.20132"
+  var query ='[out:json][timeout:25];';
+  
+  query = query + "("
+  query = query + 'relation["name"="Dorferlebnispfad Dollbergen"](' + area + ');'
+  query = query + 'node(r);'
+  query = query + ");"
+ 
+  query = query + 'out;>;out skel qt;';
+  return base_url+query;
+}
+
+
+
+// Suche
+
+var createOverpassSearchQuery = function(searchstring) {
+  var base_url= "http://overpass-api.de/api/interpreter?data="
+  var area = "52.40163, 10.16552, 52.42210, 10.20132"
+  var query ='[out:json][timeout:25];';
+  
+  query = query + "("
+  query = query + 'node["name"~"' + searchstring + '"](' + area + ');'
+  query = query + 'way["name"~"' + searchstring + '"](' + area + ');'
+  query = query + ");"
+ 
+  query = query + 'out;>;out skel qt;';
+  return base_url+query;
+}
+
+
+var searchresultlayer;
+
+var search = function(searchstring) {
+  map.removeLayer(searchresultlayer);
+  searchresultlayer = L.geoJSON(null);
+  map.addLayer(searchresultlayer);
+  
+  $.getJSON( createOverpassSearchQuery(searchstring), function(json_data ) {
+    var geojson = osmtogeojson(json_data);
+    searchresultlayer.addData(geojson);
+  });
+}
+
+
+var setSearchString = function(string) {
+  var element=document.getElementById("txtSearch");
+  element.value=string;
+}  
