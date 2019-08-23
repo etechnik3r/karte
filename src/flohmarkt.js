fb9d16100e2f7350e55cbfd870412426d8513999
Sven Klomp <mail@klomp.eu>
2019-08-23 15:03:36 +0200

    Add additional markers

--- src/flohmarkt.js
+++ src/flohmarkt.js
@@ -3,31 +3,90 @@ var layerFlohmarkt;
 var showFlohmarkt = function(show) {
   if (show) {
     if (layerFlohmarkt === undefined) {
-      layerFlohmarkt = L.geoJSON(null, {
-        style: {
-          "color": "#ff0000",
-          "weight": 5,
-          "opacity": 0.65
-        },
-        pointToLayer: function (feature, latlng) {
-          var geojsonMarkerOptions = {
-            radius: 8,
-            fillColor: "#ff0000",
-            fillOpacity: 0.7
-          };
+      
+      var icon_flohmarkt_cafe = L.icon({
+        iconUrl: 'images/marker-cafe.png',
+        iconSize: [25, 40],
+        iconAnchor: [13, 40],
+        popupAnchor: [0, -42],
+        shadowUrl: 'images/marker-shadow.png',
+        shadowSize: [41, 41],
+        shadowAnchor: [13, 41]
+      });
+      
+      var icon_flohmarkt_parking = L.icon({
+        iconUrl: 'images/marker-parking.png',
+        iconSize: [25, 40],
+        iconAnchor: [13, 40],
+        popupAnchor: [0, -42],
+        shadowUrl: 'images/marker-shadow.png',
+        shadowSize: [41, 41],
+        shadowAnchor: [13, 41]
+      });
+      
+      var icon_roadclosed = L.icon({
+        iconUrl: 'images/marker_einfahrtverboten.png',
+        iconSize: [20, 20],
+        iconAnchor: [10, 10],
+        popupAnchor: [0, -10],
+      });
 
-          var marker=L.circleMarker(latlng, geojsonMarkerOptions);
+      var icon_noparking = L.icon({
+        iconUrl: 'images/marker_halteverbot.png',
+        iconSize: [16, 16],
+        iconAnchor: [8, 8],
+        popupAnchor: [0, -10],
+      });
+
+      layerFlohmarkt = L.geoJSON(null, {
+        style: function (feature) {
+          var style = {};
+          if (feature.properties.type && feature.properties.type=="parking") {
+            style.color = "#8888ff";
+            style.weight= 3
+            style.opacity= 0.65
+            style.fillColor ="#aaaaff"
+            style.fillOpacity =0.65
+          }
+          else {
+            style.color= "#ff0000"
+            style.weight= 5
+            style.opacity= 0.65
+          }
+          return style;
+        },
+        
+        pointToLayer: function (feature, latlng) {
+
+          var marker;
           
-          if (feature.properties.hasOwnProperty("type")) {
-            if (feature.properties.type=="POI") {
-              marker=L.marker(latlng);
-              marker.bindPopup(function(layer){
-                return '<b>' + layer.feature.properties.name + '</b>';
-              });
-            }
+          if (feature.properties.type && feature.properties.type=="essen") {
+            marker=L.marker(latlng, {icon: icon_flohmarkt_cafe});
+          }
+          else if (feature.properties.type && feature.properties.type=="parking") {
+            marker=L.marker(latlng, {icon: icon_flohmarkt_parking});
+          }
+          else if (feature.properties.type && feature.properties.type=="noparking") {
+            marker=L.marker(latlng, {icon: icon_noparking});
+          }
+          else if (feature.properties.type && feature.properties.type=="roadclosed") {
+            marker=L.marker(latlng, {icon: icon_roadclosed});
+          }
+          else {
+            var geojsonMarkerOptions = {
+              radius: 8,
+              fillColor: "#ff0000",
+              fillOpacity: 0.7
+            };
+            var marker=L.circleMarker(latlng, geojsonMarkerOptions);
+          }
+
+          if (feature.properties.description) {
+            marker.bindPopup(feature.properties.description);
           }
           
-          return marker;
+          return marker;
+
         }
       });
       
@@ -35,10 +94,14 @@ var showFlohmarkt = function(show) {
 //         return '<b>' + layer.feature.properties.name + '</b>';
 //       });
       
+      $.getJSON( "data/flohmarkt_infos.geojson", function(geojson ) {
+          //var geojson = osmtogeojson(json_data);
+          layerFlohmarkt.addData(geojson);
+      });
+
       if (typeof flohmarkt_data == "undefined") {
-        flohmarkt_data="flohmarkt.geojson";
+        flohmarkt_data="flohmarkt_streets.geojson";
       }
-      
       $.getJSON( "data/" + flohmarkt_data, function(geojson ) {
           //var geojson = osmtogeojson(json_data);
           layerFlohmarkt.addData(geojson);
